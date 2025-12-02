8abe249fa77bf49e1147c70892723eaf4a4336d1
mStenz <michael.stenz2006@gmail.com>
2025-12-02 19:51:02 +0100

    day 1 still refactoring :)

--- src/main/kotlin/days/day1/Day1.kt
+++ src/main/kotlin/days/day1/Day1.kt
@@ -1,17 +1,37 @@
 package days.day1
 
 import days.Day
-import dev.stenz.algorithms.coordinate.draw
-import dev.stenz.algorithms.coordinate.mapFromStringList
+import dev.stenz.algorithms.uti.ConsoleColors
+import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.round
 
-class Day1: Day(true) {
+
+class Day1 : Day(false) {
     override fun partOne(): Any {
-        mapFromStringList(readInput()).draw()
-        return "day 1 part 1 not Implemented"
+        var dial = 50
+        return readInput().count({ line ->
+            val operation = if (line.first() == 'R') 1 else -1
+            dial = (dial + line.substring(1, line.length).toInt() * operation) % 100
+            dial == 0
+        })
     }
 
     override fun partTwo(): Any {
-        return "day 1 part 2 not Implemented"
+        var dial = 50;
+        return readInput().map { line ->
+            val operation = if (line.first() == 'R') 1 else -1
+            dial = (dial + line.substring(1, line.length).toInt() * operation)
+
+            roundToZero(dial/100.0).let { res ->
+                if (dial < 0){
+                    val result = 0
+                }
+                return@map abs(res)
+            }
+        }.sum()
     }
 }
 
+fun roundToZero(d: Double): Double = if (d < 0) ceil(d) else floor(d)

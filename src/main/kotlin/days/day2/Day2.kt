6c15b580843259f2f7a6c9f7a06c877d3345f6fb
mStenz <michael.stenz2006@gmail.com>
2025-12-02 21:22:45 +0100

    day 2 part 2

--- src/main/kotlin/days/day2/Day2.kt
+++ src/main/kotlin/days/day2/Day2.kt
@@ -7,7 +7,7 @@ import kotlin.math.floor
 import kotlin.math.log10
 import kotlin.math.pow
 
-class Day2: Day(false) {
+class Day2 : Day(false) {
     override fun partOne(): Any {
         return readInput().first()
             .split(",")
@@ -15,7 +15,7 @@ class Day2: Day(false) {
                 val parts = it.split("-").map(String::toLong)
                 parts.first() to parts.last()
             }.map { pair ->
-                println("${pair.first}-${pair.second}")
+                //println("${pair.first}-${pair.second}")
                 var num = pair.first
                 val res = mutableListOf<Long>()
                 while (num <= pair.second) {
@@ -29,20 +29,21 @@ class Day2: Day(false) {
                     while (num.getAt(i, length) == num.getAt(mid + i)) {
                         if (i == mid - 1) {
                             res.add(num) //this is a fake id heheheheheh
-                            printlnRed("FAKE: $num")
+                            //printlnRed("FAKE: $num")
                             break
                         }
                         i++
                     }
                     val diff = num.getAt(i, length) - num.getAt(mid + i)
                     if (diff <= 0) num++
-                    else num ++//= (diff * 10.0.pow(length - (mid + i) -1)).toLong()
+                    else num++//= (diff * 10.0.pow(length - (mid + i) -1)).toLong()
                 }
                 res
             }.flatten().sum()
     }
 
     override fun partTwo(): Any {
+        val mem = hashMapOf<Int, List<Int>>()
         return readInput().first()
             .split(",")
             .map {
@@ -54,33 +55,55 @@ class Day2: Day(false) {
                 val res = mutableListOf<Long>()
                 while (num <= pair.second) {
                     val length = num.length()
-
-
-                    val mid = length / 2
-                    var i = 0
-                    while (num.getAt(i, length) == num.getAt(mid + i)) {
-                        if (i == mid - 1) {
-                            res.add(num) //this is a fake id heheheheheh
-                            printlnRed("FAKE: $num")
-                            break
+                    val factors = factorsOfNumber(length, mem)
+                    if (length > 1) {
+                        numberCheck@ for (factor in factors) {
+                            val secSize = length / factor
+                            if (checkNumLegit(num, secSize, length)) {
+                                res.add(num)
+                                printlnRed("$num")
+                                break@numberCheck
+                            }
                         }
-                        i++
                     }
-                    val diff = num.getAt(i, length) - num.getAt(mid + i)
-                    if (diff <= 0) num++
-                    else num ++//= (diff * 10.0.pow(length - (mid + i) -1)).toLong()
+                    num++
                 }
                 res
             }.flatten().sum()
     }
 
+    fun checkNumLegit(num: Long, secSize: Int, length: Int): Boolean {
+        val numberOfSecs = length / secSize
+        for (i in 0..<numberOfSecs - 1) {
+            for (index in 0..<secSize) {
+                if (num.getAt(index + secSize * i) != num.getAt(index + secSize * (i + 1))) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
-    fun Long.length():Int = when(this) {
+    fun Long.length(): Int = when (this) {
         0L -> 1
         else -> log10(abs(toDouble())).toInt() + 1
     }
 
     fun Long.getAt(i: Int, length: Int? = null): Long =
         floor(this / 10.0.pow((length ?: this.length()) - i - 1.0) % 10).toLong()
+
+    fun factorsOfNumber(num: Int, mem: HashMap<Int, List<Int>>): List<Int> {
+        if (mem.containsKey(num)) return mem[num]!!
+        val factors = mutableListOf<Int>()
+        if (num < 1)
+            return factors
+        (1..num / 2)
+            .filter { num % it == 0 }
+            .filter { it != 1 }
+            .forEach { factors.add(it) }
+        factors.add(num)
+        mem[num] = factors.toList()
+        return factors
+    }
 }
 
